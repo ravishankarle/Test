f42c5f7eb7a56abb9c90f996e60b4bfa675f7af5
ravishankarle <802158ravishankar@gmail.com>
2026-03-23 15:46:48 +0530

    some change

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-function btnAdd(){
-   console.log("hit her")
+function toggleMode(){
+    document.body.classList.toggle("dark-mode");
+    document.body.classList.toggle("light-mode");
+}
+
+function showMessage(){
+    alert("Thanks for visiting my profile!");
 }
